2379919608a316acff86aca471daab17955233fa
miguetapia34-code <miguetapia34@gmail.com>
2026-09-25 00:20:06 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-import { CONFIG } from "./config.js";
+import { CONFIG } from "config.js";
 
 const form = document.querySelector("#loginForm");
 const message = document.querySelector("#loginMessage");
