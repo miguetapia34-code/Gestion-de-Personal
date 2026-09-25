ae57939bcfbd69afa344602b014510512960ac13
miguetapia34-code <miguetapia34@gmail.com>
2026-09-25 00:01:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  LOGIN_FLOW_URL: "PEGA_AQUI_LA_URL_DE_POWER_AUTOMATE",
+  LOGIN_FLOW_URL: "https://defaultec6c0d0f6a0c40b0abf2af3160580c.51.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/14/workflows/cf5703df851f42a5a60bed8c698f228b/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=eJzN8WF8g3VwgJNXBNF5vHlatIiqAbF5pDxeI4FsSH4",
   USER_SESSION_KEY: "gp_usuario"
 };
